aeda816525713a49711c59e0222b8e8c6e4245c0
Moon Seok Jun <suckjungod@naver.com>
2026-01-22 23:37:10 +0900

    [level 1] Title: 모의고사, Time: 1.00 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] p1 = {1,2,3,4,5};
+        int[] p2 = {2,1,2,3,2,4,2,5};
+        int[] p3 = {3,3,1,1,2,2,4,4,5,5};
+        
+        int[] score = new int[3];
+        
+        for (int i=0; i < answers.length; i++) {
+            if (answers[i] == p1[i % p1.length]) {
+                score[0]++;
+            }
+            if (answers[i] == p2[i % p2.length]) {
+                score[1]++;
+            }
+            if (answers[i] == p3[i % p3.length]) {
+                score[2]++;
+            }
+        }
+        
+        //이렇게 하면 3개의 최대 값을 비교할 수 있음
+        int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i =0; i <3; i++) {
+            if(score[i] == maxScore) {
+                list.add(i+1);
+            }
+        }
+        
+        int[] answer  = new int[list.size()];
+        for (int i=0; i< list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
